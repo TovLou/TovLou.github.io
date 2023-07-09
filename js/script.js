2a12b5f6abbdf15e0e6e0b47527c952653f97bc5
TovLou <12503118+TovLou@users.noreply.github.com>
2023-07-09 15:58:27 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,55 @@
+/*
+var header = $('header');
+
+var range = 200;
+
+$(window).on('scroll', function () {
+  
+  var scrollTop = $(this).scrollTop(),
+      height = header.outerHeight(),
+      offset = height / 2,
+      calc = 1 - (scrollTop - offset + range) / range;
+
+  header.css({ 'opacity': calc });
+
+  if (calc > '1') {
+    header.css({ 'opacity': 1 });
+  } else if ( calc < '0' ) {
+    header.css({ 'opacity': 0 });
+  }
+  
+});
+
+*/
+
+
+$(document).ready(function() {
+    $("#header").load("header.html");
+    $("#footer").load("footer.html");
+    $("#content-warning").load("content-warning.html");
+
+});
+
+$(window).on('scroll', function () {
+
+    var header = $('header');
+    var range = 200;
+
+    var scrollTop = $(this).scrollTop(),
+        height = header.outerHeight(),
+        offset = height / 2,
+        calc = 1 - (scrollTop - offset + range) / range;
+
+    header.css({ 'opacity': calc });
+
+    if (calc > '1') {
+        header.css({ 'opacity': 1 });
+        $('.logo-reverse').css("opacity", .8);
+        $('.logo-default').css("opacity", 0);
+    } else if ( calc < '0' ) {
+        header.css({ 'opacity': 0 });
+        $('.logo-reverse').css("opacity", 0);
+        $('.logo-default').css("opacity", .8);
+    }
+
+});
